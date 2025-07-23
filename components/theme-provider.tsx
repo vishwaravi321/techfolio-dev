08d440fca5c7f1a9ddefeb286529bb170bd973fa
vishwaravi321 <vishwaravi321@gmail.com>
2025-07-23 18:58:50 +0530

    fix: config

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import type { ThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
+/**
+ * Wrapper around `next-themes` <ThemeProvider> so we can
+ * re-export it as a named export and keep the rest of our
+ * codebase unchanged.
+ */
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
